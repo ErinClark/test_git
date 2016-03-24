3b04994b39ad080c7100c7a5a4b51876eb058f6b
erinclark <Mtkm2hds>
2016-03-24 16:23:30 +0000

    morph test

--- scraper.py
+++ scraper.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 import sys
 from bs4 import BeautifulSoup
+from splinter import Browser
 import urllib
-
+import time
 
 reload(sys)  # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 
 if __name__ == '__main__':
 
-    portal = "https://www.b2bquote.co.uk/"
-    html = urllib.urlopen(portal)
+    url = "https://www.ungm.org/Public/Notice"
+    html = urllib.urlopen(url)
+    browser = Browser("phantomjs", service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+    browser.visit(url)
+
+    browser.find_by_id('lnkClearAll').first.click()
+    time.sleep(2)
+    html = browser.html
+    soup = BeautifulSoup(html, "lxml")
+    print soup.prettify()
+    #num_tenders = soup.find('label', id="noticeSearchTotal").text
