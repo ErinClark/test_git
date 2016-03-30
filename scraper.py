aedd55b35589260a7c48863f209022a44f3dae56
erinclark <Mtkm2hds>
2016-03-30 21:11:11 +0200

    testing with all pages

--- scraper.py
+++ scraper.py
@@ -10,15 +10,7 @@ sys.setdefaultencoding('UTF8')
 
 if __name__ == '__main__':
 
-    url = "https://www.ungm.org/Public/Notice"
+    url = "http://data.gov.tw/node/7264"
     html = urllib.urlopen(url)
-    browser = Browser("phantomjs", service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
-    browser.visit(url)
-
-    browser.find_by_id('lnkClearAll').first.click()
-    time.sleep(2)
-    print browser.url
-    html = browser.html
     soup = BeautifulSoup(html, "lxml")
     print soup.prettify()
-    #num_tenders = soup.find('label', id="noticeSearchTotal").text
