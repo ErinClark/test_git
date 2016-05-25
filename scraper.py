34b6a151f31da9cc0eee537602ceb2dd665318ac
erinclark <randomgitman@gmail.com>
2016-05-25 15:57:33 +0700

    checking UTC time

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ function for country code?
 check buyer/name vs legalName
 add currency column - currently in column header
 check format of 'address' as schema split into street, locality, postcode etc
-mmp_in delete extra colum header from typo - awarding_add
+mmp_in delete extra column header from typo - awarding_add
 ungm_int :  sew together first name + last name, country calling codes + phone/fax
 
 
@@ -18,156 +18,6 @@ documents on td_e_nabavki_mk *+*+*+*+*
 philgeps: tender_url and description in codey lang - is this due do tender_url being uniquekey - therefore keeping old data + duplicating!
 
 '''
-
-# -*- coding: utf-8 -*-
-import sys
-from datetime import datetime
-from splinter import Browser
-from splinter.exceptions import ElementDoesNotExist
-import time
-import urllib
-from bs4 import BeautifulSoup
-import scraperwiki
-
-
-if __name__ == '__main__':
-
-    print 'test push'
-
-
-
-
-
-
-''' # -*- coding: utf-8 -*-
-import sys
-import json
-from splinter import Browser
-import time
-
-reload(sys) # Reload does the trick!
-sys.setdefaultencoding('UTF8')
-from datetime import datetime
-import urllib
-#import BeautifulSoup
-from bs4 import BeautifulSoup
-from bs4 import NavigableString
-
-
-def get_links_list (source_url):
-    html = urllib.urlopen(source_url)
-    soup = BeautifulSoup(html, "lxml")
-    links = soup.findAll('a', {'title':'View opportunity'})
-    return links
-
-
-def get_tender_soup (link):
-    global tender_url
-    tender_url = base_url + link['href']
-    tender_html = urllib.urlopen(tender_url)
-    tender_soup = BeautifulSoup(tender_html, "lxml")
-    return tender_soup
-
-
-def get_attr_text (tender_soup, tag, attr_type, attr_name): # where we use a tag's attributes to find previous tage and then use getText on next tag
-    if tender_soup.find(tag,{attr_type : attr_name}) == None:
-        print "+++++++++++++++  No address - aborting (No Address tag) ++++++++++++++++++++++++"
-        exit()
-    else:
-        item_name = tender_soup.find(tag,{attr_type : attr_name}).getText().strip()
-        item_name = item_name.encode('utf-8')
-    return item_name
-
-
-def get_summary():
-    try:
-        summary = tender_soup.find('dd', {"class":"synopsis"}).text
-        print summary
-    except:
-        summary = ''
-    return summary
-
-
-
-def reg_and_apply_link(link, soup):
-    try:
-        apply_status = soup.find('div', {"class":"sectionBody buttonme"}).findNext('input')['title']
-    except:
-        apply_status = ''
-    return apply_status
-
-def get_email():
-    try:
-        contact_email = tender_soup.find("dt",text="Email Address:").findNext("dd").findNext("a").contents[0]
-    except:
-        contact_email = ''
-    return contact_email
-
-if __name__ == '__main__':
-
-    portals = [
-        #'https://tender.bris.ac.uk/procontract/bristol/supplier.nsf/frm_planner_search_results?OpenForm&contains=&cats=&order_by=DATE&all_opps=CHECK&org_id=ALL',
-        'https://www.qtegov.com/procontract/supplier.nsf/frm_planner_search_results?OpenForm&contains=&cats=&order_by=DATE&all_opps=CHECK&org_id=ALL',
-    ]
-
-    for portal in portals:
-
-        print portal
-        base_url, temp = portal.split('/procontract/')
-        try:
-            links = get_links_list(portal)
-        except:
-            print 'Error getting tender links from {0}'.format(portal)
-            continue
-
-        for link in links:
-
-            linkcont = str(link.contents)
-            linkstr = str(link)
-
-            # if any(linkstr in s for s in saved_urls):
-            #     print "got tender " + linkcont + " already."
-            try:
-                tender_soup = get_tender_soup(link)  # grabs the html of a tender page and soups it.
-                tender_html = urllib.urlopen('https://www.qtegov.com/procontract/supplier.nsf/frm_opportunity?openForm&opp_id=OPP-HIS-QTLE-8M6DKG&contract_id=CONTRACT-QTLE-8M5JT3&org_id=ORG-QTLE-75FCUZ&from=')
-                tender_soup = BeautifulSoup(tender_html, "lxml")
-            except:
-                print 'Error getting tender from {0}'.format(link)
-                continue
-            print tender_url
-            summary = get_attr_text(tender_soup,"dd","class","synopsis")
-            contact_email = get_email()
-            register_and_apply_link = reg_and_apply_link(link, tender_soup)
-
-            break'''
-
-
-
-
-
-#
-
-
-'''<div class="sectionBody buttonme">
-<!--REGISTER AS SUPPLIER & INTEREST BUTTON-->
-<!--LOGIN & REGISTER INTEREST BUTTON-->
-<input class="buttonme" name="submitButton" title="Login &amp; Register Interest In This Contract" type="submit" value="Login and Register Interest"/>
-<!--REGISTER INTEREST BUTTON-->
-<a class="buttonme" href="/procontract/supplier.nsf/frm_planner_search_results?OpenForm&amp;search_id=" title="Cancel">Return to Search</a>
-</div>
-<input class="buttonme" name="submitButton" title="Login &amp; Register Interest In This Contract" type="submit" value="Login and Register Interest"/>'''
-
-
-'''<div class="sectionBody buttonme">
-<!--REGISTER AS SUPPLIER & INTEREST BUTTON-->
-<!--LOGIN & REGISTER INTEREST BUTTON-->
-<input class="buttonme" disabled="disabled" name="submitButton" title="Register Interest Window (29/09/2011 11:00 - 13/10/2011 16:00) Is Currently Closed" type="submit" value="Login &amp; Register Interest"/>
-<!--REGISTER INTEREST BUTTON-->
-<a class="buttonme" href="/procontract/supplier.nsf/frm_planner_search_results?OpenForm&amp;search_id=" title="Cancel">Return to Search</a>
-</div>
-None'''
-
-
 
 '''
 *+*+*+*+*+* REQUIREMENTS *+*+*+*+*+*+*
@@ -189,6 +39,7 @@ splinter>=0.7.3
 # -*- coding: utf-8 -*-
 import sys
 from datetime import datetime
+import datetime as dt
 from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 import time
@@ -256,7 +107,12 @@ if __name__ == '__main__':
 
     todays_date = str(datetime.now())
     country_code = ''
-    apply_requires_login = ''
+#    language =
+#    apply_requires_login =
+#    basic_details_need_login =
+#    extra_documents_with_login =
+#    tenderperiod_enddate =
+#    enddate_timezone =
     errors = []
     portal = ''
     # 'APPLY HERE LINKS'
@@ -265,6 +121,12 @@ if __name__ == '__main__':
 
               #  data = {"tender_url": unicode(tender_url),
               #          "country_code": unicode(country_code),
+              #          "language": unicode(language),
+              #          "apply_requires_login": unicode(apply_requires_login),
+              #          "basic_details_need_login": unicode(basic_details_need_login),
+              #          "extra_documents_with_login": unicode(extra_documents_with_login),
+              #          "tenderperiod_enddate": unicode(tenderperiod_enddate),
+              #          "enddate_timezone": unicode(enddate_timezone),
               #          "apply_method": unicode(apply_method),
               #          "todays_date": todays_date}
               #  scraperwiki.sqlite.save(unique_keys=['tender_url'], data=data)
@@ -274,43 +136,12 @@ if __name__ == '__main__':
 
 
 
-
-*+*+*+*+* BROWSER *+*+*+*+*+
-
-def get_browse_soup(browser):
-    html = browser.html
-    soup = BeautifulSoup(html, "lxml")
-    return soup
-
-
-def browse(url):
-    browser = Browser("phantomjs", service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
-    browser.visit(url)
-    time.sleep(2)
-    return browser
-
-
-
-
-
-
-
-*+*+*+*+*+*+*+ DATA *+*+*+*+*+*+
-
-              #  data = {"tender_url": unicode(tender_url),
-              #          "country_code": unicode(country_code),
-              #          "todays_date": todays_date}
-              #  scraperwiki.sqlite.save(unique_keys=['tender_url'], data=data)
-
-
 '''
 
-
-'''
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    driver_path = os.path.join(current_directory, 'chromedriver')
+    #current_directory = os.path.dirname(os.path.abspath(__file__))
+    #driver_path = os.path.join(current_directory, 'chromedriver')
     #print driver_path
-    #browser = Browser(driver_name='chrome', executable_path=driver_path)'''
+    #browser = Browser(driver_name='chrome', executable_path=driver_path)
 
 
 
